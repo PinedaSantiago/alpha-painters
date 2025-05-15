89bd4d8e0f73cfc4ed651b96bffbc86caf858676
PinedaSantiago <145576110+PinedaSantiago@users.noreply.github.com>
2025-05-15 15:36:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// This function runs when the page is fully loaded
+window.onload = function() {
+  // Show a welcome alert to visitors
+  alert("Welcome to Alpha Painters! We're glad you're here.");
+};
